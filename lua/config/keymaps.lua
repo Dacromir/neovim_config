a621b3c6e3543f0bdb7b83874ab28c61f4b0d328
Eric Ehninger <mailuntoeric@gmail.com>
2026-08-31 22:20:24 -0600

    Added LSP diagnostic keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,8 @@
 vim.keymap.set("n", "<leader>t", "<cmd>botright 10split | terminal<cr>i", { desc = "Open a terminal" })
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>",{ desc = "Exit terminal mode with Esc", silent = true })
 
--- Rust LSP
-vim.keymap.set("n", "<leader>rd", function()
-  vim.cmd.RustLsp('renderDiagnostic', 'current')
-end, { desc = "Show Rust diagnostic" })
+-- LSP
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic popup" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Apply recommended code action" })
+vim.keymap.set("n", "<leader>el", function() vim.diagnostic.jump({count=1,float=true}) end, { desc = "Jump to next error"})
+vim.keymap.set("n", "<leader>eh", function() vim.diagnostic.jump({count=-1,float=true}) end, { desc = "Jump to prev error"})
