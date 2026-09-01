69865a137703e371317780a8aa2afeae3fcb26c1
Eric Ehninger <mailuntoeric@gmail.com>
2026-08-31 21:55:28 -0600

    Updated terminal command to set to 10 lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- General
-vim.keymap.set("n", "<leader>t", "<cmd>botright split | terminal<cr>i", { desc = "Open a terminal" })
+vim.keymap.set("n", "<leader>t", "<cmd>botright 10split | terminal<cr>i", { desc = "Open a terminal" })
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>",{ desc = "Exit terminal mode with Esc", silent = true })
 
 -- Rust LSP
