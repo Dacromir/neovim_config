d0b0e4d18552291b38954deab1191f18c71337bd
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-02 10:38:22 -0600

    Attempted to fix theme colors for properties

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('config.options')
+require('config.pre_options')
 require('config.lazy_vim')
 require('config.keymaps')
-
+require('config.post_options')
