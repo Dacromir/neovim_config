72de74f4441f0c4ac2d7668e00e00cc1557650fb
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-01 23:22:10 -0600

    Disabled file icons in neotree

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -1,5 +1,4 @@
 return {
-  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -7,6 +6,23 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
+    config = function()
+      require('neo-tree').setup({
+        default_component_configs = {
+	  icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "󰉖",
+            folder_empty_open = "󰷏",
+            selected = "󰐾",
+            use_filtered_colors = true,
+            default = " ",
+            highlight = "NeoTreeFileIcon",
+            provider = function(icon, node, state)
+            end,
+          },
+        },
+      })
+    end,
     lazy = false, -- neo-tree will lazily load itself
-  }
 };
