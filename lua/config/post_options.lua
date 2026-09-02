5b2805f00ce971463a1bca59ed4a3f1b5989ddfd
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-02 13:53:50 -0600

    Fixed vscode theme colors for properties

--- lua/config/post_options.lua
+++ lua/config/post_options.lua
@@ -1,11 +1,1 @@
--- VS Code Theme color fixing
-local property_fg = { fg = "#9CDCFE" }
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "@property", property_fg)
-    vim.api.nvim_set_hl(0, "@variable.member", property_fg)
-    vim.api.nvim_set_hl(0, "@lsp.type.property.rust", property_fg)
-  end,
-})
--- lua/config/pre_options.lua
+++ lua/config/pre_options.lua
@@ -1,1 +1,13 @@
 vim.opt.termguicolors = true
+
+-- VS Code Theme color fixing
+local property_fg = { fg = "#9CDCFE" }
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "@property", property_fg)
+    vim.api.nvim_set_hl(0, "@variable.member", property_fg)
+    vim.api.nvim_set_hl(0, "@lsp.type.property.rust", property_fg)
+  end,
+})
