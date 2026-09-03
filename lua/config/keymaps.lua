78d54d84afbeed654be4bf0d00ad17a0f14c4b7c
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-02 20:17:28 -0600

    Added go-to-definition keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagn
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Apply recommended code action" })
 vim.keymap.set("n", "<leader>el", function() vim.diagnostic.jump({count=1,float=true}) end, { desc = "Jump to next error"})
 vim.keymap.set("n", "<leader>eh", function() vim.diagnostic.jump({count=-1,float=true}) end, { desc = "Jump to prev error"})
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition"})
+
 
 -- Move lines up and down
 vim.keymap.set('n', '<C-j>', ':m .+1<CR>==', { desc = 'Move line down' })
