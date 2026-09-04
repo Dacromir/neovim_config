c8da6e1060c05d888c2dffa558b67a99cf5acbb7
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-04 14:55:23 -0600

    WIP: Colors

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,15 +1,15 @@
 -- Colors
-local cn_gre = "#B5CEA8" -- Constants
 local black = "#1F1F1F" -- Background charcoal
 local blue1 = "#9CDCFE" -- Variable
 local blue2 = "#4FC1FF" -- Enum
 local blue3 = "#569CD6" -- Keyword
 local dgreen = "#6A9955" -- Comments
-local fn_yel = "#DCDCAA" -- Functions
+local lgreen = "#B5CEA8" -- Constants
 local lgray = "#CCCCCC" -- Punctuation
+local orange = "#CE9178" -- Strings
 local purple = "#C586C0" -- Control keywords
-local st_ora = "#CE9178" -- Strings
-local teal = "#4EC9B0"
+local teal = "#4EC9B0" -- Types
+local yellow = "#DCDCAA" -- Functions
 
 local function md_colors()
     -- Backgrounds
@@ -21,10 +21,12 @@ local function md_colors()
     vim.api.nvim_set_hl(0, "@property", {fg=blue1})
     vim.api.nvim_set_hl(0, "@variable", {fg=blue1})
     vim.api.nvim_set_hl(0, "Comment", {fg=dgreen})
-    vim.api.nvim_set_hl(0, "Constant", {fg=cn_gre})
+    vim.api.nvim_set_hl(0, "Constant", {fg=lgreen})
     vim.api.nvim_set_hl(0, "Delimiter", {fg=lgray})
-    vim.api.nvim_set_hl(0, "Function", {fg=fn_yel})
-    vim.api.nvim_set_hl(0, "String", {fg=st_ora})
+    vim.api.nvim_set_hl(0, "Function", {fg=yellow})
+    vim.api.nvim_set_hl(0, "Statement", {fg=blue3})
+    vim.api.nvim_set_hl(0, "String", {fg=orange})
+    vim.api.nvim_set_hl(0, "Type", {fg=teal})
 
     -- Lua
     vim.api.nvim_set_hl(0, "@keyword.function.lua", {fg=purple})
