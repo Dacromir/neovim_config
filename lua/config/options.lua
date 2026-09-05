873714401d3f3eb47c14c9d32cbd1c7cd81cfae5
Eric Ehninger <eric.ehninger@hq.bill.com>
2026-09-04 18:26:58 -0600

    Options: Line numbers, inded wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,12 @@
+-- Misc
+vim.opt.number = true
 vim.opt.termguicolors = true
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+-- Tabs
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Wrapping
+vim.opt.breakindent = true
+vim.opt.wrap = true
