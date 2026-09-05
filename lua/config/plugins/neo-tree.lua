c302be0310575f6dac33129de4736856d725aa31
Eric Ehninger <eric.ehninger@hq.bill.com>
2026-09-04 18:17:17 -0600

    Stop hiding in neotree based on .gitignore

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -7,22 +7,26 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     config = function()
-      require('neo-tree').setup({
-        default_component_configs = {
-	  icon = {
-            folder_closed = "",
-            folder_open = "",
-            folder_empty = "󰉖",
-            folder_empty_open = "󰷏",
-            selected = "󰐾",
-            use_filtered_colors = true,
-            default = " ",
-            highlight = "NeoTreeFileIcon",
-            provider = function(icon, node, state)
-            end,
-          },
-        },
-      })
+        require('neo-tree').setup({
+            default_component_configs = {
+	            icon = {
+                    folder_closed = "",
+                    folder_open = "",
+                    folder_empty = "󰉖",
+                    folder_empty_open = "󰷏",
+                    selected = "󰐾",
+                    use_filtered_colors = true,
+                    default = " ",
+                    highlight = "NeoTreeFileIcon",
+                    provider = function(icon, node, state) end,
+                },
+            },
+            filesystem = {
+                filtered_items = {
+                    hide_gitignored = false
+                },
+            },
+        })
     end,
     lazy = false, -- neo-tree will lazily load itself
 };
