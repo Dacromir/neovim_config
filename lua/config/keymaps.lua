edb954e34c3397ad60c17b8adaa4a469e7340213
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-06 23:38:12 -0600

    Keymap: Updated keymaps for next/previous error

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,8 +5,8 @@ vim.keymap.set('t', '<Esc>', "<C-\\><C-n>",{ desc = "Exit terminal mode with Esc
 -- LSP
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic popup" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Apply recommended code action" })
-vim.keymap.set("n", "<leader>el", function() vim.diagnostic.jump({count=1,float=true}) end, { desc = "Jump to next error"})
-vim.keymap.set("n", "<leader>eh", function() vim.diagnostic.jump({count=-1,float=true}) end, { desc = "Jump to prev error"})
+vim.keymap.set("n", "<leader>en", function() vim.diagnostic.jump({count=1,float=true}) end, { desc = "Jump to next error"})
+vim.keymap.set("n", "<leader>ep", function() vim.diagnostic.jump({count=-1,float=true}) end, { desc = "Jump to prev error"})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition"})
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
 
