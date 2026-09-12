d101fe3300275f17071fd9f95e5fc7fb81eedc56
Eric Ehninger <mailuntoeric@gmail.com>
2026-09-12 14:21:59 -0600

    Added autoformat on write autocommand

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,18 @@ vim.opt.tabstop = 4
 vim.opt.breakindent = true
 vim.opt.linebreak = true
 vim.opt.wrap = true
+
+-- Autoformat on save with LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+    callback = function(args)
+        -- Create an autocommand that runs right before a file is written to disk
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+                -- Sync format ensures the file finishes formatting before saving
+                vim.lsp.buf.format({ async = false, id = args.data.client_id })
+            end,
+        })
+    end,
+})
